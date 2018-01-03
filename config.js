3f92bced45d366cd00d505356051ee6fe050acba
sreehari <sree.bobby4u@gmail.com>
2018-01-03 23:59:51 +0530

    added database_url to config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ var exports  = module.exports = {};
 exports.secret= "vallu";
 exports.saltString ="vallu";
 exports.dbName = "budget";
-exports.mongoUrl = process.env.DB_URI || "mongodb://localhost:27017/";
+exports.mongoUrl = process.env.DB_URI || process.env.DATABASE_URL || "mongodb://localhost:27017/";
 exports.userCollection = "users";
 exports.secretToken = "vallu";
