23f55bae434d5406301f17284d6654cf25eb56b3
sreehari <sree.bobby4u@gmail.com>
2018-01-04 22:25:40 +0530

    changed config to include mlab

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ var exports  = module.exports = {};
 
 exports.secret= "vallu";
 exports.saltString ="vallu";
-exports.dbName = "budget";
-exports.mongoUrl = process.env.DB_URI || process.env.DATABASE_URL || "mongodb://localhost:27017/";
+exports.dbName = "buddybudget"//"budget";
+exports.mongoUrl = process.env.DB_URI || process.env.DATABASE_URL || 
+					"mongodb://sreeharivallu:12345678@ds139067.mlab.com:39067/" || "mongodb://localhost:27017/";
 exports.userCollection = "users";
 exports.secretToken = "vallu";
